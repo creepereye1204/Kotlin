76494ae6c436469bd7b4c26c641b3fcb3b17d9f1
creepereye1204 <creepereye1204@gmail.com>
2025-10-11 20:00:01 +0900

    init

---
+++ backend/ex0/src/main/kotlin/com/example/ex0/Ex0Application.kt
@@ -0,0 +1,11 @@
+package com.example.ex0
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Ex0Application
+
+fun main(args: Array<String>) {
+	runApplication<Ex0Application>(*args)
+}
---
+++ backend/ex0/src/test/kotlin/com/example/ex0/Ex0ApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.ex0
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class Ex0ApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
